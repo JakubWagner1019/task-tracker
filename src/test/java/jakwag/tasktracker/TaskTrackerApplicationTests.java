352a9d406d0a557eeaf3dbfb51d54cba0b2f4191
JakubWagner1019 <jakub.wagner.1019@gmail.com>
2024-10-19 13:11:21 +0200

    TASK-14 - Now contextLoads in tests doesn't fail when database is not running

--- src/test/java/jakwag/tasktracker/TaskTrackerApplicationTests.java
+++ src/test/java/jakwag/tasktracker/TaskTrackerApplicationTests.java
@@ -1,10 +1,15 @@
 package jakwag.tasktracker;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class TaskTrackerApplicationTests {
+
+    @MockBean
+    Flyway flyway;
 
     @Test
     void contextLoads() {
