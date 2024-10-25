76ba6c7bf51f4618d8488e0be94f8706a1640794
JakubWagner1019 <jakub.wagner.1019@gmail.com>
2024-10-25 11:00:22 +0200

    TASK-24 - introduced angular environments which now control baseUrl

--- task-tracker-ui/src/app/tasks/task.service.ts
+++ task-tracker-ui/src/app/tasks/task.service.ts
@@ -2,27 +2,31 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Task} from './task';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private taskUrl: string;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.taskUrl = environment.baseUrl + '/api/tasks';
+  }
 
   getAll(): Observable<Task[]> {
-    return this.http.get<Task[]>("http://localhost:8080/api/tasks");
+    return this.http.get<Task[]>(this.taskUrl);
   }
 
   getAllByStatus(status: string) : Observable<Task[]> {
-    return this.http.get<Task[]>("http://localhost:8080/api/tasks?status=" + status);
+    return this.http.get<Task[]>(this.taskUrl + "?status=" + status);
   }
 
   createTask(task: Task): Observable<void> {
-    return this.http.post<void>("http://localhost:8080/api/tasks", task);
+    return this.http.post<void>(this.taskUrl, task);
   }
 
   changeStatus(id: number, status: string): Observable<void> {
-    return this.http.patch<void>("http://localhost:8080/api/tasks/" + id, {status: status});
+    return this.http.patch<void>(this.taskUrl + '/' + id, {status: status});
   }
 }
